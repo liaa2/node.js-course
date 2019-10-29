799fb2370c9adc9a7dbbb3d17503ab4dd25a9cd1
linna <linna@MacBook-Pro.syd.dius.local>
2019-10-29 15:48:14 +1100

    https request

---
+++ playground/6-raw-http.js
@@ -0,0 +1,28 @@
+const https = require('https');
+const { DARK_SKY_API_KEY } = require('../weather-app/weather.config');
+const url = `https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/40, -75`;
+
+const request = https.request(url, (response) => {
+  // console.log("response", response);
+  
+  let data = ''
+
+  response.on('data', (chunk) => {
+    console.log(chunk);
+    data += chunk.toString();
+  })
+
+  response.on('end', () => {
+    // console.log('data', data);
+    const body = JSON.parse(data);
+    console.log(body);
+    
+  })
+})
+
+request.on('error', (error) => {
+  console.log("An error", error);
+  
+})
+
+request.end();
