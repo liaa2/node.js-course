ec3970a43d7b71f6454d938f51252604b03d435f
Linna Liu <cello1412@gmail.com>
2019-10-30 00:05:52 +1100

    added not found template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,24 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 // console.log(__dirname);
 // console.log(__filename);
 // console.log(path.join(__dirname, '../public'));
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
-app.set('view engine', 'hbs')
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs'); //setup view template engine via hbs
+app.set('views', viewsPath); //customise views path
+hbs.registerPartials(partialPath); //take the partial path to the directory
+// to keep monitoring the changes in .hbs files - run `nodemon src/app.js -e js,hbs`, '-e' stands for extension
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
@@ -22,7 +32,8 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'about page',
-    desc: 'nothing to say'
+    desc: 'nothing to say',
+    name: 'John Test',
   })
 })
 
@@ -37,13 +48,30 @@ app.get('/help', (req, res) => {
   // },
   // ]);
   res.render('help', {
-    title: 'hellllppppp'
+    title: 'Help',
+    helpText: 'THis is some helpful text',
+    name: 'Bob Test',
   })
 })
 
-// app.get('/about', (req, res) => {
-//   res.send('about');
-// })
+app.get('/help/*', (req, res) => {
+  // res.send('Help article not found');
+  res.render('error', {
+    title: 'no article',
+    errorMessage: 'Help article not found',
+    name: 'no one'
+  })
+})
+
+// * is wildcard character - match anything that hasn't match so far, need to place it after all other matches
+app.get('*', (req, res) => {
+  // res.send('My 404 page');
+  res.render('error', {
+    title: '404',
+    errorMessage: 'Page not found',
+    name: 'no one'
+  })
+})
 
 app.listen(3000, () => {
   console.log(`server is up on port ${3000}`);
