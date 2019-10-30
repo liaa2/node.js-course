f9acb699a01a8e10accaa18c5e83161aced617d9
linna <cello1412@gmail.com>
2019-10-30 12:51:25 +1100

    built get endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -12,6 +12,9 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialPath = path.join(__dirname, '../templates/partials');
 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs'); //setup view template engine via hbs
 app.set('views', viewsPath); //customise views path
@@ -52,6 +55,54 @@ app.get('/help', (req, res) => {
     helpText: 'THis is some helpful text',
     name: 'Bob Test',
   })
+})
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    res.send({
+      error: 'Error, please provide address'
+    })
+  } else {
+    // add a default value for destructed object to prevent "TypeError: Cannot destructure property `lat` of 'undefined' or 'null'"
+    geocode(req.query.address, (error, {lat, long, location} = {}) => {
+      if(error) {
+         return res.send({
+          error,
+        })
+      }
+
+      forecast(lat, long, (error, forcastData) => {
+        if (error) {
+          return res.send({
+            error,
+          })
+        } 
+        
+        res.send({
+          address: req.query.address,
+          location, 
+          forcastData,
+        })
+      })
+    });
+  }
+})
+
+app.get('/products', (req, res) => {
+  // console.log(req.query); // => { search: 'games', rating: '5' }
+  if(!req.query.search) {
+    // send back JSON object
+    res.send({
+      error: 'you must provide a search term'
+    })
+    // add else condition to prevent mulitple responds, http request should be sent once only
+  } else {
+    console.log(req.query.search);
+
+    res.send({
+      products: []
+    })
+  }
 })
 
 app.get('/help/*', (req, res) => {
